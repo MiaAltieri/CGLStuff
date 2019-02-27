b47d0799a51174d8a5711f6e320ee1b3cfe32d09
Mia Altieri <miaaltieri@eduroam-169-233-181-231.ucsc.edu>
2019-02-27 15:32:35 -0800

    basic vcf reading

---
+++ ErrorStats/ErrorStats.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 27 15:18:47 2019
+
+@author: miaaltieri
+"""
+from pysam import VariantFile
+
+# Step 1: Split the VCF into two files 
+
+vcf_in = VariantFile("test.bcf")  # auto-detect input format
+vcf_no_calls = VariantFile("NoCalls.vcf", 'w', header=vcf_in.header)
+vcf_genotype = VariantFile("NoCalls.vcf", 'w', header=vcf_in.header)
+
+for rec in vcf_in.fetch('chr1', 100000, 200000):
+    vcf_no_calls.write(rec)
+    vcf_genotype.write(rec)
